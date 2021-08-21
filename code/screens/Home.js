565aa7883c80433920821ce570c9b1c6dbd0be7e
unknown <tewedaj13@gmail.com>
2021-08-21 23:01:27 +0300

    added graph modules, and also added a sample graph, just wanted to save changes

--- code/screens/Home.js
+++ code/screens/Home.js
@@ -12,6 +12,16 @@ import {
     TouchableOpacity,
     Image
   } from 'react-native';
+  import {
+    LineChart,
+    BarChart,
+    PieChart,
+    ProgressChart,
+    ContributionGraph,
+    StackedBarChart
+  } from "react-native-chart-kit";
+  import { Dimensions } from "react-native";
+  const screenWidth = Dimensions.get("window").width;
 
   export default class Home extends React.Component{
     constructor(){ 
@@ -21,6 +31,27 @@ import {
         }
     }
 
+     data = {
+        labels: ["January", "February", "March", "April", "May", "June"],
+        datasets: [
+          {
+            data: [20, 45, 28, 80, 99, 43],
+            color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+            strokeWidth: 2 // optional
+          }
+        ],
+        legend: ["Rainy Days"] // optional
+      };
+       chartConfig = {
+        backgroundGradientFrom: "#1E2923",
+        backgroundGradientFromOpacity: 0,
+        backgroundGradientTo: "#08130D",
+        backgroundGradientToOpacity: 0.5,
+        color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+        strokeWidth: 2, // optional, default 3
+        barPercentage: 0.5,
+        useShadowColorFromDataset: false // optional
+      };
     render(){
         return(
             <View style={styles.all}>
@@ -44,7 +75,14 @@ import {
              </View>
              
             <View style={{width:'95%',height:400,backgroundColor:'black',marginTop:8}}>
-
+            <LineChart
+  data={this.data}
+  width={screenWidth}
+  height={256}
+  verticalLabelRotation={30}
+  chartConfig={this.chartConfig}
+  bezier
+/>  
             </View>
             <View style={{width:'100%',height:200,backgroundColor:'black',width:'95%',margin:20}}>
             <Text style={{fontSize:60,margin:20,fontWeight:"100",color:'white',textAlign:'center'}}>180</Text>
